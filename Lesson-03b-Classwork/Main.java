bd9df31859ff496f1930caadd4c87bae081c8eb6
Ruih5 <Ruih5@nycstudents.net>
2025-09-12 14:00:43 +0000

    1

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.println("Ruimeng Hu");
+    System.out.println("Ruimeng Hu\n7622 springfiled blvd");
+    
+  }
+
+  
+}
